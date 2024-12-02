0ead537e7426475ede538bc52517898fc67dee14
alpacaive <153301212+alpacaive@users.noreply.github.com>
2024-12-02 19:18:15 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 208 ms, Memory: 17732 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 불교연도는 서기연도의 +543년
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+        System.out.println(a - 543);
+
+    }
+}
