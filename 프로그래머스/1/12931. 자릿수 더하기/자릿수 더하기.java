a34eb1c0a6e647385bab0d4aaaef845ea919e3e8
alpacaive <153301212+alpacaive@users.noreply.github.com>
2024-12-04 02:58:07 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.04 ms, Memory: 82.9 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -10,6 +10,15 @@ public class Solution {
             answer += Integer.valueOf(N.charAt(i) - '0'); // 문자는 각각 유니코드 값 갖고있어서 '0'빼줌
         }
         
+         // 더 효율적
+         // while(true) {
+         //     answer += n % 10;
+         //     if (n < 10) {
+         //         break;
+         //     }
+         //     n = n / 10;
+         // }
+        
         return answer;
     }
 }
