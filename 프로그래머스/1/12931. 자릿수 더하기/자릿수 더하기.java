bed7709c5f85445c947737437ca4a8191f12fba0
alpacaive <153301212+alpacaive@users.noreply.github.com>
2024-12-26 19:12:39 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.04 ms, Memory: 86.6 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -1,24 +1,13 @@
-import java.util.*;
-
 public class Solution {
-    public int solution(int n) {
-        int answer = 0;
-        
-        String N = Integer.toString(n);
-        
-        for(int i = 0; i < N.length(); i++) {
-            answer += Integer.valueOf(N.charAt(i) - '0'); // 문자는 각각 유니코드 값 갖고있어서 '0'빼줌
-        }
-        
-         // 더 효율적
-         // while(true) {
-         //     answer += n % 10;
-         //     if (n < 10) {
-         //         break;
-         //     }
-         //     n = n / 10;
-         // }
-        
-        return answer;
-    }
+		public int solution(int n) {
+				int answer = 0;
+				
+				char[] arr = Integer.toString(n).toCharArray();
+				
+				for(int i = 0; i < arr.length; i++) {
+						answer += arr[i] - '0';
+				}
+				
+				return answer;
+		}
 }
