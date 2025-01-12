beb1a785ba5097718bc232c7e87e0b2b6b5394d2
alpacaive <153301212+alpacaive@users.noreply.github.com>
2025-01-13 01:19:37 +0900

    [level 1] Title: 체육복, Time: 0.59 ms, Memory: 79.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int n, int[] lost, int[] reserve) {
+        int answer = n - lost.length;
+        
+        Arrays.sort(lost);
+        Arrays.sort(reserve);
+        
+        // 여벌 체육복 있는데 도난당한 경우
+        for(int i = 0; i < lost.length; i++) {
+            for(int j = 0; j < reserve.length; j++) {
+                if(lost[i] == reserve[j]) {
+                    answer++;
+                    lost[i] = -1;
+                    reserve[j] = -1;
+                    break;
+                }
+            }
+        }
+        
+        // 자기번호 +1, -1 번호 애들한테만 대여 가능
+        for(int i = 0; i < lost.length; i++) {
+            for(int j = 0; j < reserve.length; j++) {
+                if(lost[i] - 1 == reserve[j] || lost[i] + 1 == reserve[j]) {
+                    answer++;
+                    reserve[j] = -1;
+                    break;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
