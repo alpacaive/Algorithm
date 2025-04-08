3ea899e166a89463bc219893fc65ac83c83fc5af
alpacaive <153301212+alpacaive@users.noreply.github.com>
2025-04-08 23:16:28 +0900

    [level 2] Title: [1차] 캐시, Time: 47.65 ms, Memory: 126 MB -BaekjoonHub

---
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int cacheSize, String[] cities) {
+        if(cacheSize == 0) {
+            return cities.length * 5;
+        }
+        
+        int answer = 0;
+        Queue<String> q = new LinkedList<>();
+        
+        for(int i = 0; i < cities.length; i++) {
+            String city = cities[i].toUpperCase();
+            
+            if(q.remove(city)) {
+                q.add(city);
+                answer += 1;
+            } else {
+                if(q.size() == cacheSize) {
+                    q.poll();
+                    q.add(city);
+                } else {
+                    q.add(city);
+                }
+                
+                answer += 5;
+            }
+        }
+        
+        return answer;
+    }
+}
